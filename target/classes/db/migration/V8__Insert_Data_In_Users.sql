acadc556623288c96a6467369e90c672cf28d8f8
EndrioAlberton <endrio.alberton@gmail.com>
2024-03-21 18:32:47 -0300

    Added Rest Assured, Testcontainers, and JUnit 5

--- target/classes/db/migration/V8__Insert_Data_In_Users.sql
+++ target/classes/db/migration/V8__Insert_Data_In_Users.sql
@@ -1,3 +1,3 @@
 ﻿INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
-	('leandro', 'Leandro Costa', '9700c3e73fb1bc07441089f9c6366477a668b23bc2ab1e95bcf38996c91eb4c277f3fe848acaba63', b'1', b'1', b'1', b'1'),
+	('endrio', 'Endrio Alberton', '9700c3e73fb1bc07441089f9c6366477a668b23bc2ab1e95bcf38996c91eb4c277f3fe848acaba63', b'1', b'1', b'1', b'1'),
 	('flavio', 'Flavio Costa', 'f19af3f341f27e52e83b00f4eda53a6dea24d82fc1b93c4ab5ac92498312d5d70f272ab5b433bd38', b'1', b'1', b'1', b'1');
